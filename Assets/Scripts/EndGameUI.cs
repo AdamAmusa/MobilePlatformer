bf378159317323cc60b3c277464311c4469c2438
Adam Amusa <Adamamusa2004@gmail.com>
2024-04-22 15:51:35 +0100

    Formatted

--- Assets/Scripts/EndGameUI.cs
+++ Assets/Scripts/EndGameUI.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EndGameUI : MonoBehaviour
 {
+
+    public TextMeshProUGUI coinText;
+    public TextMeshProUGUI damageText;
+
+    Coins coinObject;
+    DestroyedEnemies destroyObject;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinObject = FindObjectOfType<Coins>();
+        destroyObject = FindObjectOfType<DestroyedEnemies>();
+        coinText.text = "Coins Collected: " + coinObject.getCoins().ToString();
+        damageText.text = "Enemies Destroyed: " + destroyObject.getDestroyedEnemies().ToString();
     }
 
     // Update is called once per frame
